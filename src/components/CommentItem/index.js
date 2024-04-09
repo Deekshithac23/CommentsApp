b54d91eb1782797091ceaa9262d4819c456f4535
Deekshithac23 <ccdeekshitha123@gmail.com>
2024-04-09 16:48:46 +0000

    commit message

---
+++ src/components/CommentItem/index.js
@@ -0,0 +1,67 @@
+// Write your code here
+import {formatDistanceToNow} from 'date-fns'
+import './index.css'
+
+const CommentItem = props => {
+  const {commentDetails} = props
+  const {id, name, comment, isLiked, initialClassName, date} = commentDetails
+  const initial = name ? name[0].toUpperCase() : ''
+  const likeTextClassName = isLiked ? 'button active' : 'button'
+  const imgLikedUrl = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+  const postedTime = formatDistanceToNow(date)
+
+  onClickLike = () => {
+    const {toggleIsLiked} = props
+    toggleIsLiked(id)
+  }
+
+  onDeleteComment = () => {
+    const {deleteComment} = props
+    deleteComment(id)
+  }
+
+  return (
+    <li className="comment-item">
+      <div className="comment-container">
+        <div className={initialClassName}>
+          <p className="initial"> {initial} </p>
+        </div>
+        <div>
+          <div className="name-date-container">
+            <p className="username"> {name} </p>
+            <p className="date"> {postedTime} </p>
+          </div>
+          <p className="comment"> {comment} </p>
+        </div>
+      </div>
+      <div className="button-container">
+        <div className="like-container">
+          <img src={imgLikedUrl} alt="like" className="like-img" />
+          <button
+            type="button"
+            onClick={onClickLike}
+            className={likeTextClassName}
+          >
+            Like
+          </button>
+        </div>
+        <button
+          onClick={onDeleteComment}
+          type="button"
+          className="button"
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+      <hr className="comment-line" />
+    </li>
+  )
+}
+
+export default CommentItem
